5d84d8aeddde8b711330cd68f6944ab1c766d46b
JAY <JAY@oonne.com>
2025-03-04 16:52:38 +0800

    init: 删除内容

--- web/src/constant/pages.ts
+++ web/src/constant/pages.ts
@@ -9,42 +9,6 @@ const pages: Page[] = [
     path: '/',
     pageName: 'Index',
   },
-  {
-    path: '/base64',
-    pageName: 'Base64',
-  },
-  {
-    path: '/qrcode',
-    pageName: 'QRcode',
-  },
-  {
-    path: '/md5',
-    pageName: 'MD5',
-  },
-  {
-    path: '/sha',
-    pageName: 'SHA',
-  },
-  {
-    path: '/aes',
-    pageName: 'AES',
-  },
-  {
-    path: '/rsa',
-    pageName: 'RSA',
-  },
-  {
-    path: '/scrypt',
-    pageName: 'SCRYPT',
-  },
-  {
-    path: '/pbkdf2',
-    pageName: 'PBKDF2',
-  },
-  {
-    path: '/password',
-    pageName: 'Password',
-  },
 ];
 
 export default pages;
--- web/src/main.ts
+++ web/src/main.ts
@@ -3,16 +3,6 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import pages from './constant/pages';
 import App from './App.vue';
 
-import HeaderBar from './components/HeaderBar.vue';
-import ContentCard from './components/ContentCard.vue';
-import TabsNav from './components/TabsNav.vue';
-import ConfirmButton from './components/ConfirmButton.vue';
-import TextInput from './components/TextInput.vue';
-import ValueInput from './components/ValueInput.vue';
-import SelectInput from './components/SelectInput.vue';
-import CheckboxInput from './components/CheckboxInput.vue';
-import FileInput from './components/FileInput.vue';
-
 const app = createApp(App);
 
 /* 路由 */
@@ -36,14 +26,3 @@ const router = createRouter({
   routes,
 });
 app.use(router).mount('#app');
-
-/* 全局组件 */
-app.component('HeaderBar', HeaderBar);
-app.component('ContentCard', ContentCard);
-app.component('TabsNav', TabsNav);
-app.component('ConfirmButton', ConfirmButton);
-app.component('TextInput', TextInput);
-app.component('ValueInput', ValueInput);
-app.component('SelectInput', SelectInput);
-app.component('CheckboxInput', CheckboxInput);
-app.component('FileInput', FileInput);
